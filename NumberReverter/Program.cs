e6f7adc6d73288208a7feb264723c49a3ff16b5c
Artem Horbunov <Artem_Horbunov1@epam.com>
2022-08-16 21:36:36 +0300

    Add existing tasks to one solution

---
+++ NumberReverter/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Enter number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+string result = "";
+int a = num % 10;
+num = num / 10;
+result += a;
+
+a = num % 10;
+num = num / 10;
+result += a;
+
+a = num % 10;
+num = num / 10;
+result += a ;
+
+a = num % 10;
+num = num / 10;
+result += a;
+
+a = num % 10;
+num = num / 10;
+result += a;
+
+a = num % 10;
+num = num / 10;
+result += a;
+
+Console.WriteLine(result);
